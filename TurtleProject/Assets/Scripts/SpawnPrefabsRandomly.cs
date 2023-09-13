840705c773875938b7b56410df1f64d6ebaf4f06
stefanovinaccia <142214109+stefanovinaccia@users.noreply.github.com>
2023-09-13 14:48:14 +0200

    cambiamenti

--- TurtleProject/Assets/Scripts/SpawnPrefabsRandomly.cs
+++ TurtleProject/Assets/Scripts/SpawnPrefabsRandomly.cs
@@ -3,36 +3,124 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPrefabsRandomly : MonoBehaviour
-{
+{ 
+    
     public GameObject prefabToSpawn1;
     public GameObject prefabToSpawn2;
     public GameObject prefabToSpawn3;
     public float spawnAreaWidth = 100f; // Larghezza dell'area in cui verranno spawnati i prefabs
     public float spawnAreaLength = 100f; // Lunghezza dell'area in cui verranno spawnati i prefabs
     public int numerodaspawnare =10;
+    public float originepianox;
+    public  float originepianoz;
+    public float lunghezzapiano;
+    public float larghezzapiano;
+    private static int oggettiNelPiano ;
+    public List<GameObject> spazzature = new List<GameObject>();
+    public Rigidbody rb ;
+    public float upwardForce;
+    
+   // public Text testo;
+    public float totalTime = 60f; // Il tempo totale del timer in secondi.
+    private float currentTime;
+    
     void Start()
-    {
-        SpawnPrefabs();
+    { 
+       
+           oggettiNelPiano = 0;
+           currentTime = totalTime;
+          Transform tp = GetComponent<Transform>();
+          originepianox=tp.position.x;
+          originepianoz=tp.position.z;
+         
+          SpawnPrefabs();
     }
+    void Update(){
+        // Debug.Log(oggettiNelPiano);
+         currentTime -= Time.deltaTime;
+       // testo.text = Mathf.Round(currentTime).ToString();
+         /*if (oggettiNelPiano==numerodaspawnare||a==1){
+             a=1;
+            rb.AddForce(Vector3.up * upwardForce);
+         }*/
+         if (currentTime <= 0)
+        {
+            currentTime = 0;
+             rb.AddForce(Vector3.up * upwardForce);
+            // Esegui azioni quando il timer scade.
+            // Ad esempio, puoi terminare il gioco o attivare un'altra logica.
+        }
+
+          /*  if (oggettiNelPiano==numerodaspawnare)
+            {
+                Debug.Log("spazzatura raccolta");
+             foreach (GameObject spazzatura in spazzature)
+            {
+                Destroy(spazzatura);
+            }
+
+            
+            spazzature.Clear();
+            }*/
+
+
+        
+
+    }
+  
 
     void SpawnPrefabs()
     {
+       
         for (int i = 0; i < numerodaspawnare; i++) // Numero di prefabs da spawnare
         {
-            Vector3 randomPosition = new Vector3( Random.Range(-spawnAreaWidth / 2, spawnAreaWidth / 2),Random.Range(80f,100f), Random.Range(-spawnAreaLength / 2, spawnAreaLength / 2 ));
-
-            if (i<3){
-              Instantiate(prefabToSpawn1, randomPosition, Quaternion.identity);  
+            Vector3 randomPosition = new Vector3( originepianox+lunghezzapiano,Random.Range(80f,100f), Random.Range(originepianoz-spawnAreaLength/2,originepianoz+spawnAreaLength / 2 ));
+            Vector3 randomPosition1 = new Vector3( Random.Range(originepianox-spawnAreaLength/2,originepianox+spawnAreaLength / 2 ),Random.Range(80f,100f), originepianoz+lunghezzapiano);
+            Vector3 randomPosition2 = new Vector3( originepianox-lunghezzapiano,Random.Range(80f,100f), Random.Range(originepianoz-spawnAreaLength/2,originepianoz+spawnAreaLength / 2 ));
+            Vector3 randomPosition3 = new Vector3( Random.Range(originepianox-spawnAreaLength/2,originepianox+spawnAreaLength / 2 ),Random.Range(80f,100f), originepianoz-lunghezzapiano);
+            
+            if (i<2){
+                if(i%2==0){
+                    spazzature.Add(Instantiate(prefabToSpawn1, randomPosition, Quaternion.identity));
+                }else{
+              spazzature.Add(Instantiate(prefabToSpawn1, randomPosition1, Quaternion.identity));  
+                }
             }
-            else if(i>=3&&i<6){
-                Instantiate(prefabToSpawn2, randomPosition, Quaternion.identity);
+            else if(i>=2&&i<4){
+                if(i%2==0){
+                    spazzature.Add(Instantiate(prefabToSpawn2, randomPosition2, Quaternion.identity));
+
+                }else{
+                        spazzature.Add(Instantiate(prefabToSpawn2, randomPosition3, Quaternion.identity));
+                }
+                  
             }
             else{
-                Instantiate(prefabToSpawn3, randomPosition, Quaternion.identity);
+                spazzature.Add(Instantiate(prefabToSpawn3, randomPosition, Quaternion.identity));
             }
            
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("spazzatura"))
+        {
+            oggettiNelPiano++;
+            //Debug.Log("Oggetto entrato nel piano. Oggetti totali nel piano: " + oggettiNelPiano);
+        }
+      
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("spazzatura"))
+        {
+            oggettiNelPiano--;
+           // Debug.Log("Oggetto uscito dal piano. Oggetti totali nel piano: " + oggettiNelPiano);
+        }
+    }
+      
+    
+     
 }
 
 

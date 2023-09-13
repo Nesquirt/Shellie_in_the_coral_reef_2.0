82351682de73446c75ecd02ae9f881584a56dfd4
Nesquirt <sg5027@gmail.com>
2023-09-13 03:42:39 +0200

    Aggiunto Sistema Monete Aggiunto sistema monete come script separato per maggiore comodità usando singleton per verificare che solo un oggetto SistemaMonete sia presente nel gioco, a fine file aggiunti commenti per il richiamo della funzione da script esterni. Aggiunto TextMeshPro a lato del canvas per prova, da spostare nel canvas e visualizzarlo. Aggiorna quanto chiamata una delle due funzioni per aggiungere o rimuovere perle

---
+++ TurtleProject/Assets/Scripts/SistemaMonete.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SistemaMonete : MonoBehaviour
+{
+    private static SistemaMonete _istanza;
+
+    public static SistemaMonete Istanza
+    {
+        get
+        {
+            if (_istanza == null)
+            {
+                _istanza = FindObjectOfType<SistemaMonete>();
+
+                if (_istanza == null)
+                {
+                    // Se l'istanza non esiste, crea un nuovo oggetto vuoto e aggiungi il sistema di monete ad esso
+                    GameObject sistemaMoneteObject = new GameObject("SistemaMonete");
+                    _istanza = sistemaMoneteObject.AddComponent<SistemaMonete>();
+                }
+            }
+            return _istanza;
+        }
+    }
+
+    public int perle = 0; // La quantità di perle iniziale
+    public TextMeshProUGUI testoPerle; // Riferimento al Text UI per le perle
+
+    private void Awake()
+    {
+        if (_istanza != null && _istanza != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _istanza = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Funzione per aggiungere perle
+    public void AggiungiPerle(int quantita)
+    {
+        perle += quantita;
+        AggiornaTestoPerle();
+    }
+
+    // Funzione per sottrarre perle
+    public void SottraiPerle(int quantita)
+    {
+        perle -= quantita;
+        AggiornaTestoPerle();
+    }
+
+    // Funzione per aggiornare il testo delle perle
+    void AggiornaTestoPerle()
+    {
+        if (testoPerle != null)
+        {
+            testoPerle.text = "Perle: " + perle.ToString();
+        }
+    }
+}
+
+
+
+
+// SistemaMonete.Istanza.AggiungiPerle(quantitaDaAggiungere); //aggiungere perle da altri script
+// SistemaMonete.Istanza.SottraiPerle(quantitaDaSottrarre);  // rimuovere perle da altri script
+// int quantitaPerle = SistemaMonete.Istanza.perle;  //verificare quantità attuale di perle da altro script
+
+
+// made with love from Assassin's script ♥
+
+
+
+

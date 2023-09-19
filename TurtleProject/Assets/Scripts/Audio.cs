156f08deee538cb1833415b27712873b9597473d
LoaLunare <francesco.maria.picchi@gmail.com>
2023-09-19 20:02:31 +0200

    Audio

--- TurtleProject/Assets/Scripts/Audio.cs
+++ TurtleProject/Assets/Scripts/Audio.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 
 public class Audio : MonoBehaviour
 {
-    [Header("- - - - - AUDIO SOURCE - - - - - ")]
+    [Header("- - - - AUDIO SOURCE - - - - - ")]
     [SerializeField] AudioSource SoundtrackSource;
     [SerializeField] AudioSource MovementSource;
-    [SerializeField] AudioSource SFXSource;
+    [SerializeField] AudioSource BubbleSource;
+    [SerializeField] AudioSource SFX2DSource;
+    [SerializeField] AudioSource SFX3DSource;
 
-    [Header("- - - - - AUDIO CLIP - - - - - ")]
-    public AudioClip bubble;
+    [Header("- - - - - AUDIO CLIP - - - - - - ")]
     public AudioClip soundtrack;
+    public AudioClip movement;
+    public AudioClip bubble;
 
     private void Start()
     {
         SoundtrackSource.clip = soundtrack;
         SoundtrackSource.Play();
-        MovementSource.clip = bubble;
+        MovementSource.clip = movement;
         MovementSource.Play();
+        BubbleSource.clip = bubble;
+        BubbleSource.Play();
     }
 }

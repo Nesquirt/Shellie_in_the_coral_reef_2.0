f4a0e29056df8148aec61c054258c7d6c4a927c3
LoaLunare <francesco.maria.picchi@gmail.com>
2023-09-20 17:51:02 +0200

    Modifche audio

--- TurtleProject/Assets/Scripts/TargetHandler.cs
+++ TurtleProject/Assets/Scripts/TargetHandler.cs
@@ -46,7 +46,7 @@ public class TargetHandler : MonoBehaviour
         confirmButton.onClick.AddListener(ConfirmButton_onClick);
         cancelButton.onClick.AddListener(CancelButton_onClick);
 
-        //audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         
     }
     // -------------------------------------------------------------------- //
@@ -171,7 +171,7 @@ public class TargetHandler : MonoBehaviour
                 if(targetNumber == 0)
                 {
                     StartCoroutine(Timer());
-                    //audioManager.PlaySFX(audioManager.startRace);
+                    audioManager.PlaySFX(audioManager.startRace);
 
             }
                 if(targetNumber == 28)
@@ -179,13 +179,13 @@ public class TargetHandler : MonoBehaviour
                     Victory();
                     StopCoroutine(Timer());
                     timer.gameObject.SetActive(false);
-                    //audioManager.PlaySFX(audioManager.endRace);
+                    audioManager.PlaySFX(audioManager.endRace);
                     return;
                 }
                 GameObject.Find(nextTargetName).GetComponent<MeshRenderer>().material = activeMaterial;
                 if(targetNumber > 0 && targetNumber < 28)
                 {
-                    //audioManager.PlaySFX(audioManager.crossRing);
+                    audioManager.PlaySFX(audioManager.crossRing);
                 }
 
             targetNumber++;

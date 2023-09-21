d75f4cd9b2ca06abe3509bac42e886f9f57797b9
Nesquirt <sg5027@gmail.com>
2023-09-21 15:22:01 +0200

    Import AudioManager

--- TurtleProject/Assets/Scripts/AudioManager.cs
+++ TurtleProject/Assets/Scripts/AudioManager.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class AudioManager : MonoBehaviour
 {
     [Header("- - - - AUDIO SOURCE")]
     [SerializeField] AudioSource SoundtrackSource;
+    [SerializeField] AudioSource MinigamesSoundtrackSource;
     [SerializeField] AudioSource MovementSource;
     [SerializeField] AudioSource BubbleSource;
     [SerializeField] AudioSource GrowingCoralSource;
@@ -21,17 +24,20 @@ public class AudioManager : MonoBehaviour
     public AudioClip coral;
     public AudioClip selection;
 
-    [Header("- - - - AUDIO CLIP MATTEO'S GAME (ONE SHOT)")]
+    [Header("- - - - AUDIO CLIP MATTEO'S GAME")]
+    public AudioClip MatteoGameSountrack;
     public AudioClip startRace;
     public AudioClip endRace;
     public AudioClip crossRing;
     public AudioClip Crush_Spawn;
 
-    [Header("- - - - AUDIO CLIP SARA'S GAME (ONE SHOT)")]
+    [Header("- - - - AUDIO CLIP SARA'S GAME")]
+    public AudioClip SaraGameSountrack;
     public AudioClip KeyTaken;
     public AudioClip CageOpening;
 
-    [Header("- - - - AUDIO CLIP STEFANO'S GAME (ONE SHOT)")]
+    [Header("- - - - AUDIO CLIP STEFANO'S GAME")]
+    public AudioClip StefanoGameSountrack;
     public AudioClip GridDrop;
     public AudioClip TrashMoving;
     public AudioClip ShipHorn;
@@ -51,9 +57,24 @@ public class AudioManager : MonoBehaviour
         SFXSource.clip = sfx;
         SFXSource.Play();
     }
-    /*public void PlaySoundtrack(AudioClip sountrack)
+    public void ChangeMusic(AudioClip miniGameSoundtrack)
     {
-        SoundtrackSource.clip = sountrack;
+        SoundtrackSource.clip = miniGameSoundtrack;
         SoundtrackSource.Play();
-    }*/
+        StartCoroutine(Fade());
+    }
+    public IEnumerator Fade()
+    {
+        float time = 0f;
+        float duration = 5f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            SoundtrackSource.volume = Mathf.Lerp(0.2f, 0, time / duration);
+            MinigamesSoundtrackSource.volume = Mathf.Lerp(0, 0.2f, time / duration);
+            yield return null;
+        }
+        yield break;
+    }
+
 }
